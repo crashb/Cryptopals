8cdf8c73bbb3e22a5d36a8fbb8b198abd849649e
crash <snickerless1@shaw.ca>
2017-12-03 16:07:31 -0700

    completed Challenge 9

---
+++ Set 2/Challenge9.py
@@ -0,0 +1,16 @@
+# solution to http://cryptopals.com/sets/1/challenges/9
+# implementation of byte padding
+
+# padBytes takes a bytearray of bytes to pad, as well as a block size to pad to
+# returns a bytearray of padded bytes
+def padBytes(bytesToPad, blockSize):
+	numPadBytes = blockSize - (len(bytesToPad) % blockSize)
+	paddedBytes = bytesToPad
+	for i in range(0, numPadBytes):
+		paddedBytes.append(4)
+	return paddedBytes
+
+if __name__ == "__main__":
+	someBytes = bytearray("YELLOW SUBMARINE", "ascii")
+	paddedBytes = padBytes(someBytes, 20)
+	print("Padded bytes: " + str(paddedBytes))
