ed44ec3a3c611215b0683027668f67a7ce186e59
crash <snickerless1@shaw.ca>
2017-12-04 23:22:54 -0700

    completed challenge 12

--- Set 2/Challenge12.py
+++ Set 2/Challenge12.py
@@ -24,12 +24,6 @@ def padBytes(bytesToPad, blockSize):
 	for i in range(0, numPadBytes):
 		paddedBytes.append(4)
 	return paddedBytes
-
-# decrypts AES cipher in ECB mode.  arguments are cipherBytes (bytes) and key (bytes)
-# returns plaintext (bytes)
-def decryptAES_ECB(cipherBytes, keyBytes):
-	cipher = AES.new(keyBytes, AES.MODE_ECB)
-	return cipher.decrypt(cipherBytes)
 	
 # encrypts AES cipher in ECB mode.  arguments are plainBytes (bytes) and key (bytes)
 # returns ciphertext (bytes)
@@ -45,20 +39,27 @@ def encryptAES_ECB(plainBytes, keyBytes):
 def encryptionProcess(givenBytes):
 	plainBytes = bytearray(givenBytes)
 	# append unknown plaintext bytes to plaintext
-	with open('Challenge10Data.txt', 'r') as myfile:
-		pass
+	with open('Challenge12Data.txt', 'r') as myfile:
 		plainBytes += base64.b64decode(''.join(myfile.read().strip().split('\n')))
 	plainBytes = padBytes(plainBytes, len(randomKey))
 	cipherBytes = encryptAES_ECB(bytes(plainBytes), bytes(randomKey))
 	return cipherBytes
 
+	
+#      above this line: setting up the problem
+#-----------------------------------------------------
+#       below this line: solving the problem
+
 # 1. find the block length of the encryption process
 # returns block length (int)
 def findProcessBlockLength():
+	# get an initial ciphertext value for when no bytes are provided
 	testBytes = bytearray()
 	initialCiphered = encryptionProcess(testBytes)
-	# try block lengths from 1 - 32
-	for i in range(1, 33):
+	# try block lengths from 3 - 32 (avoid 1 and 2 because they can return false positives easily)
+	testBytes.append(0)
+	testBytes.append(0)
+	for i in range(3, 33):
 		testBytes.append(0)
 		tryCiphered = encryptionProcess(testBytes)
 		# if the first block of the initial cipher equals the second block of the cipher we are trying,
@@ -70,7 +71,7 @@ def findProcessBlockLength():
 	return -1
 	
 # 2. find the AES mode of the encryption process given blockLength (int)
-# returns either "ECB" or "CBC"
+# returns either "ECB" or "Not found"
 def detectProcessMode(blockLength):
 	# chosen string is twice the size of the block length and all 'a'
 	chosenBytes = bytearray()
@@ -88,42 +89,84 @@ def detectProcessMode(blockLength):
 	print("Error: not able to detect ECB mode")
 	return "Not found"
 
-# 3. calls encryption process with a chosen plaintext that is exactly one byte short.
+# 3. call encryption process with a chosen plaintext that is exactly one byte short
+# called with block length (int), the block number we are looking at (int), and the byte we are looking at within that block (int)
 # returns the first block (bytearray)
-def getShortenedInputBlock(blockLength):
-	# chosen string is one byte short of the block length and all 'a'
+def getShortenedInputBlock(blockLength, blockNo, byteNo):
+	# chosen string will be one byte short of the block length, minus the length of known plaintext in this block (byteNo)
+	# consists of all 'a'
 	chosenBytes = bytearray()
-	for i in range(0, blockLength - 1):
+	for i in range(0, blockLength - byteNo - 1):
 		chosenBytes.append(ord('a'))
 	cipherBytes = encryptionProcess(chosenBytes)
-	# return last byte of block
-	return cipherBytes[0:blockLength]
+	# return whichever block of ciphertext we are looking at
+	return cipherBytes[blockNo*blockLength:(blockNo+1)*blockLength]
 	
-# 4. makes dictionary of every possible last byte
-# returns dictionary of ciphertext blocks and plaintext byte (bytearray, int)
-def getPossibilities(blockLength):
+# 4. make dictionary of every possible last byte
+# called with block length (int), any known plaintext from this block (bytearray), and the previous block's plaintext (bytearray)
+# returns dictionary of ciphertext block and plaintext byte (bytearray, int)
+def getPossibilities(blockLength, knownPlaintext, lastBlockPlaintext):
+	# the chosen string will be precisely one block length long
+	chosenBytePrefix = bytearray()
+	# if in the first block (the last block's plaintext is an empty bytearray):
+	# start with all 'a', then add any known plaintext to a length of (blockLength - 1)
+	if lastBlockPlaintext == bytearray():
+		for i in range(0, blockLength - len(knownPlaintext) - 1):
+			chosenBytePrefix.append(ord('a'))
+		for i in range(0, len(knownPlaintext)):
+			chosenBytePrefix.append(knownPlaintext[i])
+	# if in any other block:
+	# add the previous (blockLength - 1) bytes of known plaintext, some of which are
+	# from the last block and some of which are from this block
+	else:
+		for i in range(len(knownPlaintext) + 1, blockLength):
+			chosenBytePrefix.append(lastBlockPlaintext[i])
+		for i in range(0, len(knownPlaintext)):
+			chosenBytePrefix.append(knownPlaintext[i])
+			
+	# by this point, chosenBytePrefix is only missing one byte at the end of it: the variable byte value.
+	# iterate through all possible byte values to end our chosen string with, and populate dictionary with results
 	possibilities = {}
 	for i in range(0, 256):
-		# chosen string one block length, all 'a' but ends with i (byte value)
-		chosenBytes = bytearray()
-		for j in range(0, blockLength - 1):
-			chosenBytes.append(ord('a'))
+		chosenBytes = bytearray(chosenBytePrefix)
 		chosenBytes.append(i)
 		cipherBytes = encryptionProcess(chosenBytes)
-		# add entry to dictionary
 		possibilities[cipherBytes[0:blockLength]] = i
-		# print("Set " + str(cipherBytes[0:blockLength]) + " to " + str(i))
 	return possibilities
+
+# get a single block of plaintext
+# takes block length (int), the number of the block to be obtained (int), and the last plaintext block (bytearray)
+# returns the block of plaintext (bytearray)
+def getPlaintextBlock(blockLength, blockNo, lastPlainBlock):
+	plainBlock = bytearray()
+	for i in range(0, blockLength):
+		shortenedInputBlock = getShortenedInputBlock(blockLength, blockNo, len(plainBlock))
+		# print("Result from shortened block: " + str(shortenedInputBlock) + "(Length: " + str(len(shortenedInputBlock)) + ")")
+		bytePossi = getPossibilities(blockLength, plainBlock, lastPlainBlock)
+		plainByte = bytePossi[shortenedInputBlock]
+		# print("Plaintext byte: " + str(plainByte) + " (ASCII: " + chr(plainByte) + ")")
+		plainBlock.append(plainByte)
+	return plainBlock
+
+# find the plaintext message hidden in the encryptionProcess function, given the block length (int)
+# returns the plaintext message (bytearray)
+def getPlaintextMessage(blockLength):
+	messageLength = len(encryptionProcess(bytearray()))
+	message = bytearray()
+	lastPlainBlock = bytearray()
+	numBlocks = int(messageLength / blockLength)
+	for i in range(0, numBlocks):
+		plainBlock = getPlaintextBlock(blockLength, i, lastPlainBlock)
+		message += plainBlock
+		lastPlainBlock = plainBlock
+	return message
 
 if __name__ == "__main__":
 	blockLength = findProcessBlockLength()
 	print("Block length of encryption process: " + str(blockLength))
 	encMode = detectProcessMode(blockLength)
 	print("Mode of encryption process: " + encMode)
-	shortenedInputBlock = getShortenedInputBlock(blockLength)
-	print("Shortened input block (encrypted): " + str(shortenedInputBlock))
-	allBytes = getPossibilities(blockLength)
-	# print("Possibilities: " + str(allBytes))
-	firstPlainByte = allBytes[shortenedInputBlock]
-	print("First plaintext byte (decrypted): " + str(firstPlainByte) + " (ASCII: " + chr(firstPlainByte) + ")")
+	print("Decrypting message...")
+	messageBytes = getPlaintextMessage(blockLength)
+	print(messageBytes.decode("ascii"))
 	
