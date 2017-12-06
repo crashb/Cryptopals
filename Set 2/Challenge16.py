967e31f549d124eba5541d901e8aaa5c4a707285
crash <snickerless1@shaw.ca>
2017-12-06 14:44:01 -0700

    completed challenge 16

--- Set 2/Challenge16.py
+++ Set 2/Challenge16.py
@@ -30,17 +30,16 @@ def getEncodedProfile(inputString):
 	encodedCipherBytes = Challenge10.encryptAES_CBC(bytes(encodedPlainBytes), randomKey, bytes(randomIV))
 	return encodedCipherBytes
 
-# decrypts provided bytes with CBC, removes padding
+# decrypts provided bytes with CBC
 # returns encoded plaintext (bytes)
 def decryptProfile(cipherBytes):
-	plainBytes = Challenge10.decryptAES_CBC(bytes(cipherBytes), randomKey, bytes(randomIV))
-	plainBytes = Challenge15.unpadBytes(plainBytes, blockLength)
-	return plainBytes
+	return Challenge10.decryptAES_CBC(bytes(cipherBytes), randomKey, bytes(randomIV))
 	
 # given ciphered encoded bytes, decrypt them and check for string ";admin=true;"
 # returns boolean
 def checkAdmin(cipherBytes):
 	plainBytes = decryptProfile(cipherBytes)
+	print("Decrypted bytes: \n" + str(plainBytes))
 	if bytearray(";admin=true;", "ascii") in plainBytes:
 		print("Admin entry found!")
 		return True
@@ -53,12 +52,44 @@ def checkAdmin(cipherBytes):
 #-----------------------------------------------------
 #       below this line: solving the problem
 
+
+# calculates the block of ciphertext needed to generate chosen plaintext
+# takes the desired plaintext bytes (bytearray), the block number the plaintext will go in (int), and entirety of the ciphertext (bytearray), 
+# this means that the block of ciphertext returned by this function will go in position (blockNo - 1)
+# returns ciphertext block (bytearray)
+def getNewBlock(desiredPlainBytes, blockNo, cipherBytes):
+	# we cannot inject more than a block of plaintext at once
+	if len(desiredPlainBytes) > blockLength:
+		raise ValueError("Cannot fit " + int(len(desiredPlainBytes)) + " plaintext bytes into a block size of " + int(blockLength))
+
+	# get padded plaintext bytes when user input is "" (assume this is known)
+	plainBytes = bytearray("comment1=cooking MCs;userdata=;comment2= like a pound of bacon", "ascii")
+	plainBytes = Challenge15.padBytes(plainBytes, blockLength)
+	# get the block of plaintext that we want to overwrite with our chosen plaintext
+	plainBlock = plainBytes[blockNo*blockLength:(blockNo+1)*blockLength]
+	# get the block of ciphertext that we are going to modify to produce the modified plaintext
+	cipherBlock = cipherBytes[(blockNo-1)*blockLength:blockNo*blockLength]
+	
+	for i in range(0, len(desiredPlainBytes)):
+		# generate mask by xoring current plaintext byte and desired plaintext byte.
+		# this mask holds the bits we need to flip in the ciphertext ('1' means we need to flip)
+		mask = desiredPlainBytes[i] ^ plainBlock[i]
+		# now, XOR'ing the mask with the current ciphertext byte yields the modified ciphertext byte
+		cipherBlock[i] ^= mask
+	return cipherBlock
+		
+
 if __name__ == "__main__":
-	inputString = "butts"
+	inputString = ""
 	print("Creating profile with input string \"" + inputString + "\"...")
 	encodedCipherBytes = getEncodedProfile(inputString)
-	print("Encoded ciphertext bytes: " + str(encodedCipherBytes))
-	checkAdmin(encodedCipherBytes)
-	encodedPlainBytes = decryptProfile(encodedCipherBytes)
-	print("Encoded plaintext bytes: " + str(encodedPlainBytes))
+	print("Initial ciphertext bytes: \n" + str(encodedCipherBytes))
 	
+	desiredBytes = bytearray(";admin=true;", "ascii")
+	blockPos = 2
+	newBlock = getNewBlock(desiredBytes, blockPos, encodedCipherBytes)
+	# modified block goes in position (blockPos - 1)
+	moddedCipherBytes = encodedCipherBytes[0:(blockPos-1)*blockLength] + newBlock + encodedCipherBytes[blockPos*blockLength:]
+	print("Modified ciphertext bytes: \n" + str(moddedCipherBytes))
+	
+	checkAdmin(moddedCipherBytes)
