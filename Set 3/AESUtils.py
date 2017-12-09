92a231235090bec18b550399e4105bc1433a4792
crash <snickerless1@shaw.ca>
2017-12-09 14:44:13 -0700

    completed challenge 17

---
+++ Set 3/AESUtils.py
@@ -0,0 +1,86 @@
+# from Set 2/Challenge 10
+# implementation of AES CBC mode
+
+import base64
+from Crypto.Cipher import AES
+
+# decrypts AES cipher in ECB mode.  arguments are cipherBytes (bytes) and key (bytes)
+# returns plaintext (bytes)
+def decryptAES_ECB(cipherBytes, keyBytes):
+	cipher = AES.new(keyBytes, AES.MODE_ECB)
+	return cipher.decrypt(cipherBytes)
+	
+# encrypts AES cipher in ECB mode.  arguments are plainBytes (bytes) and key (bytes)
+# returns ciphertext (bytes)
+def encryptAES_ECB(plainBytes, keyBytes):
+	cipher = AES.new(keyBytes, AES.MODE_ECB)
+	return cipher.encrypt(plainBytes)
+
+# decrypts AES cipher in CBC mode.  chains together AES_ECB decryptions and XORs blocks together
+# more information: https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_Block_Chaining_.28CBC.29
+# takes cipherBytes (bytearray), keyBytes (bytearray), and initialization vector bytes (bytearray)
+# returns bytearray of decrypted bytes
+def decryptAES_CBC(cipherBytes, keyBytes, IVBytes):
+	numBlocks = int(len(cipherBytes) / len(keyBytes))
+	xorBytes = IVBytes
+	decrypted = bytearray()
+	for i in range(0, numBlocks):
+		encryptedBlock = cipherBytes[i*len(keyBytes):(i+1)*len(keyBytes)]
+		nextXorBytes = encryptedBlock
+		decryptedBlock = decryptAES_ECB(bytes(encryptedBlock), bytes(keyBytes))
+		xoredBlock = fixedXOR(bytearray(decryptedBlock), xorBytes)
+		decrypted += xoredBlock
+		xorBytes = nextXorBytes
+	return decrypted
+	
+# encrypts AES cipher in CBC mode.  chains together AES_ECB encryptions and XORs blocks together
+# more information: https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_Block_Chaining_.28CBC.29
+# takes plainBytes (bytearray), keyBytes (bytearray), and initialization vector bytes (bytearray)
+# returns bytearray of encrypted bytes
+def encryptAES_CBC(plainBytes, keyBytes, IVBytes):
+	numBlocks = int(len(plainBytes) / len(keyBytes))
+	xorBytes = IVBytes
+	encrypted = bytearray()
+	for i in range(0, numBlocks):
+		plainBlock = plainBytes[i*len(keyBytes):(i+1)*len(keyBytes)]
+		xoredBlock = fixedXOR(plainBlock, xorBytes)
+		encryptedBlock = encryptAES_ECB(bytes(xoredBlock), bytes(keyBytes))
+		xorBytes = encryptedBlock
+		encrypted += encryptedBlock
+	return encrypted
+
+# getEmptyIV is a function to return an empty initialization vector of a given length (int)
+# returns bytearray of [0, 0, 0, ...]
+def getEmptyIV(length):
+	IV = bytearray();
+	for i in range(0, length):
+		IV.append(0)
+	return IV
+
+# XORs two bytearrays
+# returns a bytearray that is the result of the operation
+def fixedXOR(buffer1Bytes, buffer2Bytes):
+	xorResultBytes = bytearray(buffer1Bytes)
+	for index, byte in enumerate(buffer2Bytes):
+		xorResultBytes[index] ^= byte
+	return xorResultBytes
+	
+# tests the CBC encryption function by encrypting and decrypting a supplied plaintext string using a supplied key string
+# prints encryption and decryption results
+# returns nothing - only purpose is to print results
+def testEncryptAES_CBC(plainString, keyString):
+	print("Testing CBC mode encryption on string \"" + plainString + "\" with key \"" + keyString + "\"...")
+	plainBytes = bytearray(plainString, "ascii")
+	keyBytes = bytearray(keyString, "ascii")
+	encryptedBytes = encryptAES_CBC(plainBytes, keyBytes, getEmptyIV(len(keyString)))
+	print("Encrypted bytes:")
+	print(str(encryptedBytes))
+	decryptedBytes = decryptAES_CBC(encryptedBytes, keyBytes, getEmptyIV(len(keyString)))
+	print("Decrypted bytes:")
+	print(str(decryptedBytes))
+	# check if we obtain the expected result
+	if (decryptedBytes.decode("ascii") == plainString[0:len(decryptedBytes)]) and (decryptedBytes.decode("ascii") != ""):
+		print("The decrypted string matches the supplied string - CBC mode is working!")
+	else:
+		print("The decrypted string does not match the supplied string - check the results manually.")
+	return
---
+++ Set 3/Challenge17.py
@@ -0,0 +1,122 @@
+# solution to http://cryptopals.com/sets/3/challenges/17
+# CBC bitflipping / padding oracle attack
+
+import base64
+import random
+import AESUtils
+import paddingUtils
+
+# generates a random sequence of bytes.  length is determined by length argument (int).
+# returns randomBytes (bytearray)
+def randomByteGen(length):
+	randomBytes = bytearray()
+	for i in range(0, length):
+		randomBytes.append(random.randint(0, 255))
+	return randomBytes
+	
+# set blocklength to 16 - assume it is known
+blockLength = 16
+# generate unknown random key and IV as a global
+randomKey = randomByteGen(blockLength)
+randomIV = randomByteGen(blockLength)
+
+# given a filename holding 10 different lines of base64 encoded text 
+# returns ciphertext bytes (bytearray)
+def getCiphertext(fileName):
+	plainList = []
+	with open(fileName, 'r') as myfile:
+		for line in myfile:
+			plainList.append(line.strip())
+	plainText = random.choice(plainList)
+	plainBytes = base64.b64decode(''.join(plainText.strip().split('\n')))
+	plainBytes = paddingUtils.padBytes(plainBytes, blockLength)
+	cipherBytes = AESUtils.encryptAES_CBC(plainBytes, randomKey, randomIV)
+	return cipherBytes
+	
+# returns true or false depending on if a ciphertext's padding is valid
+# returns boolean
+def validPadding(cipherBytes):
+	try:
+		plainBytes = AESUtils.decryptAES_CBC(cipherBytes, randomKey, randomIV)
+		plainBytes = paddingUtils.unpadBytes(plainBytes, blockLength)
+		return True
+	except ValueError as e:
+		# print(str(e))
+		return False
+
+
+#      above this line: setting up the problem
+#-----------------------------------------------------
+#       below this line: solving the problem
+
+# The fundamental insight behind this attack is that the byte 01h is valid padding, and occur 
+# in 1/256 trials of "randomized" plaintexts produced by decrypting a tampered ciphertext.
+
+# finds one byte of plaintext by exploiting the padding oracle.
+# needs two blocks of ciphertext: lastCipherBlock (bytearray), which is modified to decode a byte in cipherBlock (bytearray)
+# the function also requires the number of the byte we are looking at (counting from the end of the block, int 1-16) as
+# well as any known bytes from the block that have already been decrypted
+# returns a byte of plaintext (int)
+def findPlainByte(lastCipherBlock, cipherBlock, byteNo, knownBytes):
+	# we are going to supply two blocks of ciphertext to the oracle:
+	# the first block is the tampered ciphertext, and the second block is what we want to decode
+	origByte = lastCipherBlock[-byteNo]
+	blockPrefix = lastCipherBlock[:-byteNo]
+	# set up suffix
+	blockSuffix = bytearray()
+	for i in range(0, len(knownBytes)):
+		blockSuffix.append(knownBytes[i] ^ byteNo ^ lastCipherBlock[-(len(knownBytes) - i)])
+	# start creating different ciphertexts to send
+	for i in range(0, 256):
+		newBlock = bytearray(blockPrefix)
+		newBlock.append(i)
+		newBlock += blockSuffix
+		# now that we have our tampered block, add the desired ciphertext to it
+		newBlock += cipherBlock
+		if (validPadding(newBlock)):
+			# edge case for if we are finding the final byte of the final block: there are
+			# two cases where it can have valid padding, and one of them is with the original
+			# byte.  we want to get rid of that possibility, so we just skip it.
+			if i == origByte and byteNo == 1:
+				continue
+			# XORing the original ciphertext byte (origByte) with the decryption result
+			# will yield the plaintext character, so:
+			# if the padding is valid, we know that the plaintext byte = the supplied byte 
+			# XORed with the original ciphertext byte and the padding number (byteNo)
+			plainByte = i ^ origByte ^ byteNo
+			# print("Valid byte: " + str(plainByte) + " (" + chr(plainByte) + ")")
+			return plainByte
+	print("No valid bytes found!")
+	return -1
+	
+# find a full block of plaintext by exploiting the padding oracle.
+# needs two blocks of ciphertext: lastCipherBlock (bytearray), which is modified to decode cipherBlock (bytearray)
+# returns a plaintext block (bytearray)
+def findPlainBlock(lastCipherBlock, cipherBlock):
+	plainBytes = bytearray()
+	for i in range(0, blockLength):
+		newByte = findPlainByte(lastCipherBlock, cipherBlock, i + 1, plainBytes)
+		plainBytes.insert(0, newByte)
+	# print("Decrypted block: " + str(plainBytes))
+	return plainBytes
+
+# decrypts a message that was encrypted with AES CBC encryption.
+# takes the ciphertext bytes (bytearray)
+# returns the plaintext bytes (bytearray)
+def findPlainMsg(cipherBytes):
+	print("Decrypting message...")
+	numBlocks = int(len(cipherBytes) / blockLength)
+	lastCipherBlock = bytearray(randomIV)
+	plainBytes = bytearray()
+	# print("Initialization vector: " + str(randomIV))
+	for i in range(0, numBlocks):
+		cipherBlock = cipherBytes[i*blockLength:(i+1)*blockLength]
+		plainBytes += findPlainBlock(lastCipherBlock, cipherBlock)
+		lastCipherBlock = cipherBlock
+	return plainBytes
+	
+if __name__ == "__main__":
+	cipherText = getCiphertext("Challenge17Data.txt")
+	plainMessage = findPlainMsg(cipherText)
+	plainMessage = paddingUtils.unpadBytes(plainMessage, blockLength)
+	print("Decrypted message: " + plainMessage.decode("ascii"))
